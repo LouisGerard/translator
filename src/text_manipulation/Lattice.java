95b0ea5deaa5e146ae26f25edd2caa5b14066dfe
Louis GERARD <louis.gerardd@gmail.com>
2017-11-06 11:40:57 +0100

    viterbi [untested]

--- src/text_manipulation/Lattice.java
+++ src/text_manipulation/Lattice.java
@@ -5,8 +5,11 @@ import stats.Stats2Gram;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import static jdk.nashorn.internal.objects.Global.Infinity;
 
 public class Lattice {
     private HashMap<Integer, HashMap<Integer, Double>> alpha;
@@ -19,18 +22,33 @@ public class Lattice {
         read(path);
     }
 
-    /*public List<Integer> viterbi() {
-        double max;
+    public List<Integer> viterbi() {
+        List<Integer> result = new ArrayList<>();
+        int max;
+        HashMap<Integer, HashMap<Integer, Double>> chances = stats.getChances();
         for (int col = 1; col < alpha.size(); ++col) {
             for (int line = 0; line < alpha.get(col).size(); ++line) {
                 max = 0;
-                int index = tokens.get(col).get(line);
+                int index2 = tokens.get(col).get(line);
 
-                double value = alpha.get(col-1).get(max) + stats.getChances().get()
-                alpha.get(col).put(index, 0.0);
+                int k = 0;
+                double alphaValue = Infinity;
+                do {
+                    int index1 = tokens.get(col - 1).get(k);
+                    double alphaTmp = alpha.get(col - 1).get(k) + chances.get(index1).get(index2) + alpha.get(col).get(line);
+                    if (alphaTmp < alphaValue) {
+                        alphaValue = alphaTmp;
+                        max = k;
+                    }
+                } while (k++ < alpha.get(col - 1).size());
+
+                beta.get(col).put(line, max);
+                result.add(tokens.get(col - 1).get(max));
+                alpha.get(col).put(line, alphaValue);
             }
         }
-    }*/
+        return result;
+    }
 
     private void read(String path) throws IOException {
         int colNum = 0;
@@ -42,9 +60,9 @@ public class Lattice {
             if (line == null)
                 break;
 
-            int colIndex = line.indexOf("%col");
-            if (colIndex == 0) {
-                colNum = Integer.parseInt(line.substring(colIndex + 1));
+            if (line.indexOf("%col") == 0) {
+                int cutIndex = line.indexOf(' ');
+                colNum = Integer.parseInt(line.substring(cutIndex + 1));
                 alpha.put(colNum, new HashMap<>());
                 beta.put(colNum, new HashMap<>());
                 tokens.put(colNum, new HashMap<>());
@@ -55,8 +73,8 @@ public class Lattice {
             int cutIndex = line.indexOf(' ');
             int index = Integer.parseInt(line.substring(0, cutIndex));
             double alphaTmp = Double.parseDouble(line.substring(cutIndex + 1));
-            alpha.get(colNum).put(index, alphaTmp);
-            beta.get(colNum).put(index, 0);
+            alpha.get(colNum).put(lineNum, alphaTmp);
+            beta.get(colNum).put(lineNum, 0);
             tokens.get(colNum).put(lineNum++, index);
         }
     }
